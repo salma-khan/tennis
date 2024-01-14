820f6308658151448f73f5ffb25e58274c9e03c0
salma-khan <salmakhanchouch@MacBook-Air-de-Salma.local>
2024-01-14 18:57:13 +0100

    format code.

--- src/main/java/com/tennis/bforbank/Points.java
+++ src/main/java/com/tennis/bforbank/Points.java
@@ -1,22 +1,19 @@
 package com.tennis.bforbank;
 
 public enum Points {
-    _0( "0"),
-    _15( "15"),
-    _30("30"),
-    _40("40");
+    _0("0"), _15("15"), _30("30"), _40("40");
 
-    private String name;
+    private final String name;
 
-    Points(String name){
+    Points(String name) {
         this.name = name;
+    }
+
+    public static Points from(int value) {
+        return Points.values()[value];
     }
 
     public String getName() {
         return name;
-    }
-
-    public static Points from(int value) {
-        return  Points.values()[value];
     }
 }
--- src/main/java/com/tennis/bforbank/Tennis.java
+++ src/main/java/com/tennis/bforbank/Tennis.java
@@ -3,65 +3,65 @@ package com.tennis.bforbank;
 public class Tennis {
 
     private static final String SCORE_PRINTS = "Player A: %s / Player B: %s";
-    private static final char PLAYER_A ='A';
-    private static final char PLAYER_B ='B';
+    private static final char PLAYER_A = 'A';
+    private static final char PLAYER_B = 'B';
 
     private int scoreA;
     private int scoreB;
 
 
-    public  void startGame(String sequence){
-        if(sequence.length()==0||!sequence.chars().allMatch(c->c== PLAYER_A || c== PLAYER_B)){
+    public void startGame(String sequence) {
+        if (sequence.length() == 0 || !sequence.chars().allMatch(c -> c == PLAYER_A || c == PLAYER_B)) {
             throw new IllegalArgumentException("Only player A and B are valid entries");
         }
         System.out.println(sequence);
         final char[] game = sequence.toCharArray();
 
-        for(char currentPlayer: game){
-            if(isPlayerA(currentPlayer)){
+        for (char currentPlayer : game) {
+            if (isPlayerA(currentPlayer)) {
                 scoreA++;
-            } else{
+            } else {
                 scoreB++;
             }
 
-            if(isDeuce()){
+            if (isDeuce()) {
                 System.out.println("The game is in DEUCE");
                 continue;
             }
 
-            if(isAdvantageFor(currentPlayer)){
-                System.out.println(String.format("ADV %c", currentPlayer));
+            if (isAdvantageFor(currentPlayer)) {
+                System.out.printf("ADV %c%n", currentPlayer);
                 continue;
             }
 
-            if(isWinner(currentPlayer)){
-                System.out.println(String.format("Winner is %c", currentPlayer));
+            if (isWinner(currentPlayer)) {
+                System.out.printf("Winner is %c%n", currentPlayer);
                 break;
             }
 
-            System.out.println(String.format(SCORE_PRINTS, Points.from(scoreA).getName(), Points.from(scoreB).getName()));
+            System.out.printf((SCORE_PRINTS) + "%n", Points.from(scoreA).getName(), Points.from(scoreB).getName());
         }
 
     }
 
 
-    public boolean isDeuce(){
-        return scoreA == scoreB && scoreA >=3;
+    public boolean isDeuce() {
+        return scoreA == scoreB && scoreA >= 3;
     }
 
-    public boolean isAdvantageForPlayerA(){
+    public boolean isAdvantageForPlayerA() {
         return isAdvantageFor(PLAYER_A);
     }
 
-    public boolean isAdvantageForPlayerB(){
-       return  isAdvantageFor(PLAYER_B);
+    public boolean isAdvantageForPlayerB() {
+        return isAdvantageFor(PLAYER_B);
     }
 
-    public boolean isWinner(char playerName){
-        if(playerName == PLAYER_A){
-            return scoreA> 3 &&  scoreA >=scoreB+2;
+    public boolean isWinner(char playerName) {
+        if (playerName == PLAYER_A) {
+            return scoreA > 3 && scoreA >= scoreB + 2;
         } else {
-            return  scoreB>3 && scoreB>=scoreA+2;
+            return scoreB > 3 && scoreB >= scoreA + 2;
         }
     }
 
@@ -73,16 +73,16 @@ public class Tennis {
         return Points.from(scoreB).getName();
     }
 
-    private boolean isAdvantageFor(char player){
-        if(player == PLAYER_A) {
+    private boolean isAdvantageFor(char player) {
+        if (player == PLAYER_A) {
             return scoreA > 3 && scoreA == scoreB + 1;
         }
 
-        return scoreB>3 && scoreB == scoreA + 1;
+        return scoreB > 3 && scoreB == scoreA + 1;
     }
 
-    private boolean isPlayerA(char playerName){
-        return  playerName == PLAYER_A;
+    private boolean isPlayerA(char playerName) {
+        return playerName == PLAYER_A;
     }
 
 }
